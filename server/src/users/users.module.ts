2cd3664556a351d0f19d27af475f38163cd85834
Christopher Rush <ceo@christopherhustle.com>
2022-10-06 18:44:06 +0200

    [CAR-1] ADD User and Report entities

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { User } from "./user.entity";
 
-@Module({
-  controllers: [UsersController],
-  providers: [UsersService]
-})
-export class UsersModule {}
+@Module( {
+  imports: [
+    TypeOrmModule.forFeature( [ User ] )
+  ],
+  controllers: [ UsersController ],
+  providers: [ UsersService ]
+} )
+export class UsersModule {
+}

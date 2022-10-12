8a52ca75e9f933534ac883c8bd45dbf28e4f25e6
Christopher Rush <ceo@christopherhustle.com>
2022-10-12 21:15:24 +0200

    [CAR-2] ADD interceptor to return dto

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,8 +1,21 @@
-import { Controller, Body, Post, Get, Delete, Patch, Param, Query, NotFoundException } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Post,
+  Get,
+  Delete,
+  Patch,
+  Param,
+  Query,
+  NotFoundException
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
+@Serialize( UserDto )
 @Controller( 'auth' )
 export class UsersController {
   
@@ -18,7 +31,7 @@ export class UsersController {
   async findUser( @Param( 'id' ) id: string ) {
     const user = await this.usersService.findOne( parseInt( id ) );
     if ( !user ) {
-      throw new NotFoundException('user not found');
+      throw new NotFoundException( 'user not found' );
     }
     return user;
   }
@@ -35,7 +48,7 @@ export class UsersController {
   
   @Patch( '/:id' )
   updateUser( @Param( 'id' ) id: string, @Body() body: UpdateUserDto ) {
-    return this.usersService.update( parseInt(id), body );
+    return this.usersService.update( parseInt( id ), body );
   }
   
 }

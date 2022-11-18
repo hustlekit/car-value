5a681ed4938a1a87f6a1c8236d5fa83b40566492
Christopher Rush <ceo@christopherhustle.com>
2022-11-18 16:38:16 +0100

    [CAR-2] ADD signin and signup routes, session dependencies

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from "@nestjs/common";
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cookieSession = require( 'cookie-session' );
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create( AppModule );
+  app.use( cookieSession( {
+    keys: [ 'reallysecretkey' ],
+  } ) );
   app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true
-    })
+    new ValidationPipe( {
+      whitelist: true,
+    } ),
   );
-  await app.listen(3000);
+  await app.listen( 3000 );
 }
+
 bootstrap();
--- server/src/users/dtos/create-user.dto.ts
+++ server/src/users/dtos/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsString, isString } from "class-validator";
+import { IsEmail, IsString } from "class-validator";
 
 export class CreateUserDto {
 

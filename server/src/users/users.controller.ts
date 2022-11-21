72ae47f74ecedeb377ef49f1cdf0bfce5f3dd23b
Christopher Rush <ceo@christopherhustle.com>
2022-11-21 17:56:59 +0100

    [CAR-2] ADD global interceptor and route guard

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,13 +1,15 @@
 import {
-  Controller,
   Body,
-  Post,
-  Get,
+  Controller,
   Delete,
-  Patch,
+  Get,
+  NotFoundException,
   Param,
+  Patch,
+  Post,
   Query,
-  NotFoundException, Session,
+  Session,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -15,9 +17,13 @@ import { UsersService } from './users.service';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from './user.entity';
+import { AuthGuard } from '../guards/auth.guard';
 
-@Serialize( UserDto )
+
 @Controller( 'auth' )
+@Serialize( UserDto )
 export class UsersController {
   
   constructor(
@@ -26,13 +32,14 @@ export class UsersController {
   ) {
   }
   
-  @Get('/whoami')
-  whoAmI(@Session() session: any) {
-    return this.usersService.findOne(session.userId);
+  @Get( '/whoami' )
+  @UseGuards(AuthGuard)
+  whoAmI( @CurrentUser() user: User ) {
+    return user;
   }
   
-  @Post('/signout')
-  signOut(@Session() session: any) {
+  @Post( '/signout' )
+  signOut( @Session() session: any ) {
     session.userId = null;
   }
   

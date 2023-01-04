d8f91b7617b446f3568dcefe4988c9cc5f4d2a15
Christopher Rush <ceo@christopherhustle.com>
2023-01-04 23:16:04 +0100

    [CAR-4] ADD user - report relation

---
+++ server/src/reports/dtos/report.dto.ts
@@ -0,0 +1,32 @@
+import { Expose, Transform } from 'class-transformer';
+
+export class ReportDto {
+  
+  @Expose()
+  id: number;
+  
+  @Expose()
+  pricec: number;
+  
+  @Expose()
+  year: number;
+  
+  @Expose()
+  lng: number;
+  
+  @Expose()
+  lat: number;
+  
+  @Expose()
+  make: string;
+  
+  @Expose()
+  model: string;
+  
+  @Expose()
+  mileage: number;
+  
+  @Transform( ( { obj } ) => obj.user.id )
+  @Expose()
+  userId: number;
+}

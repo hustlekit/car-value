13e88dfb1ab940f1f79831568abadcf3a0332f37
Christopher Rush <ceo@christopherhustle.com>
2023-01-05 15:36:19 +0100

    [CAR-5] ADD admin guard, current user middleware instead of interceptor

--- server/src/reports/reports.controller.ts
+++ server/src/reports/reports.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Param, Patch, Post, UseGuards, Get, Query } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
@@ -7,6 +7,8 @@ import { User } from '../users/user.entity';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
 import { ApproveReportDto } from './dtos/approve-report.dto';
+import { AdminGuard } from '../guards/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller( 'reports' )
 export class ReportsController {
@@ -14,6 +16,11 @@ export class ReportsController {
   constructor(
     private reportsService: ReportsService,
   ) {
+  }
+  
+  @Get()
+  getEstimate( @Query() query: GetEstimateDto ) {
+    console.log(query);
   }
   
   @Post()
@@ -24,7 +31,8 @@ export class ReportsController {
   }
   
   @Patch( '/:id' )
+  @UseGuards( AdminGuard )
   approveReport( @Param( 'id' ) id: string, @Body() body: ApproveReportDto ) {
-    return this.reportsService.changeApproval(id, body.approved);
+    return this.reportsService.changeApproval( id, body.approved );
   }
 }
